2d4dd90db9e5545e349a125af700965989408333
m-waris <m.warismahmood@gmail.com>
2023-07-13 18:23:34 +0500

    Hero Section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 function App() {
   return (
     <>
     <Navbar />
+    <Hero />
     </>
     
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Typewriter  from 'typewriter-effect'
+
+export default function Hero() {
+  return (
+    <div className="hero-wrapper">
+        <div className="heroInfo">
+            <h1>Full Stack Web Developer</h1>
+            <Typewriter  
+            onInit={(typewriter) => {
+                typewriter.typeString('Hello! I am a Full Stack Web Developer')
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString('My name is <strong>Muhammad</strong>')
+                .pauseFor(500)
+                .deleteChars(8)
+                .typeString('<strong>Waris</strong>')
+                .pauseFor(500)
+                .deleteChars(5)
+                .typeString('<strong>Mahmood</strong>')
+                .pauseFor(500)
+                .deleteAll()
+                .start();
+                }}
+            />
+        </div>
+  
+    </div>
+  )
+}
